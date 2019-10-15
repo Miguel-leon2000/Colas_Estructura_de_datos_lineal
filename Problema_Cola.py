0f21838c500e639de73f3aa232f3c51d3139cc34
Miguel Angel <leonaguirrem3@gmail.com>
2019-10-14 22:51:20 -0500

    feat: Se modificó el proceso para generar los numeros negativos en una nueva lista, asignandolo en el metodo "Recorrer", mientras que el proceso para generar numeros aleatorios se basó en el metodo "AgregarFinal", se agregaron comentarios al programa.

--- Problema_Cola.py
+++ Problema_Cola.py
@@ -1,33 +1,34 @@
 import random  #Se importa la libreria random para generar numeros aleatorios
 
-class GenerarNumeros():
-
-    def NumerosAleatorios(self):
-        c = []
-        for a in range(0, 100):
-            b = random.randint(-25, 25)
-            c.append(b)
-        print(c)
-
-        d = []
-        for x in range(0, len(c)):
-            if c[x] < 0:
-                d.append(c[x])
-        print(d)
-
+"""
+Se define la clase nodo en el que toma como parametro el dato (en el metodo constructor)
+que en este caso esta vacio y por consiguiente su nodo siguiente es nulo.
+"""
 class Nodo():
     def __init__(self, dato):
         self.dato = dato
         self.siguiente = None
 
+"""
+Se agrega la clase ListaCircular en el que no tiene un principio ni un fin, 
+es por eso que se le agrega nulo al primero y al ultimo nodo.
+"""
 class ListaCircular():
     def __init__(self):
         self.primero = None
         self.ultimo = None
 
+    """
+    Metodo "Vacio" en el que indica si la lista esta vacia,
+    regresa que el primer nodo no tiene elementos.
+    """
     def Vacia(self):
         return self.primero == None
 
+    """
+    Metodo para asignar un dato al inicio del nodo,
+    agregando una condicion para saber si la lista esta vacia y si no realiza las instrucciones.
+    """
     def AgregarInicio(self, dato):
         if self.Vacia():
             self.primero = self.ultimo = Nodo(dato)
@@ -38,6 +39,10 @@ class ListaCircular():
             self.primero = aux
             self.ultimo.siguiente = self.primero
 
+    """
+    Metodo para mandar llamar al primer nodo para que asi 
+    la lista se considere circular.
+    """
     def AgregarFinal(self, dato):
         if self.Vacia():
             self.primero = self.ultimo = Nodo(dato)
@@ -47,14 +52,29 @@ class ListaCircular():
             self.ultimo = aux.siguiente = Nodo(dato)
             self.ultimo.siguiente = self.primero
 
+    """
+    Metodo para realizar el recorrido, en el que se implementa las instrucciones para
+    generar una nueva lista en el que se le van agregar los valores negativos de la lista original.
+    """
     def Recorrer(self):
         aux = self.primero
+        Cola_negativa = "Los numero negativos son: "
+        negativos = ListaCircular()
+
         while aux:
-            print(aux.dato)
+            print(aux, aux.dato, aux.siguiente)
+            if aux.dato < 0:
+                negativos.AgregarFinal(aux.dato)
+                Cola_negativa = str(Cola_negativa) + str(aux.dato)
             aux = aux.siguiente
             if aux == self.primero:
                 break
+        print(Cola_negativa)
 
+    """
+    Metodo para remover el inicio del nodo, considerando tambien 
+    si la lista esta vacia mediante una condicion if.
+    """
     def RemoverInicio(self):
         if self.Vacia():
             print("Lista vacia")
@@ -64,6 +84,10 @@ class ListaCircular():
             self.primero = self.primero.siguiente
             self.ultimo.siguiente = self.primero
 
+    """
+    Metodo para remover el fin del nodo, en el que tambien se le aplica 
+    la condicion para saber si la lista esta vacia.
+    """
     def RemoverFin(self):
         if self.Vacia():
             print("Lista vacia")
@@ -76,5 +100,15 @@ class ListaCircular():
             aux.siguiente = self.primero
             self.primero = aux
 
-Generar = GenerarNumeros
-Generar.NumerosAleatorios(GenerarNumeros)
+
+Lista = ListaCircular()  #Se le asigna el nombre al objeto de la clase "ListaCircular()"
+
+"""
+Se implementa un ciclo for, en el que se le agrega un tamaño de 100 numeros aleatorios,
+mandando llamar al metodo "AgregarFinal" agregandolo asi a una nueva lista considerando la variable c
+y por ultimo se realiza el recorrido.
+"""
+for a in range(1, 100):
+    c = random.randint(-25, 25)
+    Lista.AgregarFinal(c)
+Lista.Recorrer()
