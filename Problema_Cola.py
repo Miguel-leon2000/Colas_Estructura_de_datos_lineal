f77b25d9d1ce275b3a4af0fd16e6de416a7739df
Miguel Angel <leonaguirrem3@gmail.com>
2019-10-14 21:48:53 -0500

    feat: Se implementaron metodos de las operaciones de listas circulares y se creo un metodo para crear una nueva lista con los numeros negativos de la lista original.

--- Problema_Cola.py
+++ Problema_Cola.py
@@ -1,14 +1,80 @@
-import random  #Se importa la libreria ramdom para generar numeros aleatorios
+import random  #Se importa la libreria random para generar numeros aleatorios
 
-class GenerarNumeros(): #Se crea la clase
+class GenerarNumeros():
 
-    """
-    Ciclo for en el que toma un tamaño de 100 numeros aleatorios,
-    en el rango de -25 a 25
-    """
-    for a in range(0, 100):
-        contador = a  #Se agrega un contador para cada elemento (para verificar)
-        b = random.randint(-25, 25)
-        print(b)
-        contador += 1
-        print("Numero: ", contador)
+    def NumerosAleatorios(self):
+        c = []
+        for a in range(0, 100):
+            b = random.randint(-25, 25)
+            c.append(b)
+        print(c)
+
+        d = []
+        for x in range(0, len(c)):
+            if c[x] < 0:
+                d.append(c[x])
+        print(d)
+
+class Nodo():
+    def __init__(self, dato):
+        self.dato = dato
+        self.siguiente = None
+
+class ListaCircular():
+    def __init__(self):
+        self.primero = None
+        self.ultimo = None
+
+    def Vacia(self):
+        return self.primero == None
+
+    def AgregarInicio(self, dato):
+        if self.Vacia():
+            self.primero = self.ultimo = Nodo(dato)
+            self.ultimo.siguiente = self.primero
+        else:
+            aux = Nodo(dato)
+            aux.siguiente = self.primero
+            self.primero = aux
+            self.ultimo.siguiente = self.primero
+
+    def AgregarFinal(self, dato):
+        if self.Vacia():
+            self.primero = self.ultimo = Nodo(dato)
+            self.ultimo.siguiente = self.primero
+        else:
+            aux = self.ultimo
+            self.ultimo = aux.siguiente = Nodo(dato)
+            self.ultimo.siguiente = self.primero
+
+    def Recorrer(self):
+        aux = self.primero
+        while aux:
+            print(aux.dato)
+            aux = aux.siguiente
+            if aux == self.primero:
+                break
+
+    def RemoverInicio(self):
+        if self.Vacia():
+            print("Lista vacia")
+        elif self.primero == self.ultimo:
+            self.primero = self.ultimo = None
+        else:
+            self.primero = self.primero.siguiente
+            self.ultimo.siguiente = self.primero
+
+    def RemoverFin(self):
+        if self.Vacia():
+            print("Lista vacia")
+        elif self.primero == self.ultimo:
+            self.primero = self.ultimo = None
+        else:
+            aux = self.primero
+            while aux.siguiente != self.ultimo:
+                aux = aux.siguiente
+            aux.siguiente = self.primero
+            self.primero = aux
+
+Generar = GenerarNumeros
+Generar.NumerosAleatorios(GenerarNumeros)
